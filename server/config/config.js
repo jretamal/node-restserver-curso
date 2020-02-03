5d2a4183d97c3b6b3491c9167bc46eb31219647f
jretamal <jretamal@gmail.com>
2020-02-03 08:40:18 -0300

    Se genero variable de entorno con URI

--- server/config/config.js
+++ server/config/config.js
@@ -15,8 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb+srv://nodejs-cafe:nodejs-cafe@cluster0-7xfrn.mongodb.net/test?retryWrites=true&w=majority";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
